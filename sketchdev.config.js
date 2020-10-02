36c17a8112b31d977649958d69f8f3b9b9faffac
Jeremy Chone <jeremy.chone@gmail.com>
2020-10-02 16:45:28 -0700

    . initial from github.com:dom-native/quickstart v0.1.8

---
+++ sketchdev.config.js
@@ -0,0 +1,29 @@
+
+
+module.exports = {
+	// the origin for the input (design.sketch)
+	origin: 'https://raw.githubusercontent.com/jeremychone/sketch-files/main/design-quickstart.sketch',
+	// download origin to input if not present (otherwise, needs to do 'npm run sketchdev download' explicitly)
+	download: true,
+
+	input: 'design.sketch',
+
+	output: [{
+		type: 'svg',
+		out: 'svg/sprite.svg',
+		artboard: /^ico\/.*/,
+		flatten: '-'
+	},
+	{
+		type: 'png',
+		out: './',
+		artboard: /^images\/.*/
+	},
+	{
+		type: 'style',
+		out: 'pcss/var-colors.pcss',
+		style: /^clr\/.*/,
+		group: 2
+	}
+	]
+}
