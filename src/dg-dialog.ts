1a414465fb7431c8da4c69cb20d07eb1133160dd
Jeremy Chone <jeremy.chone@gmail.com>
2020-10-02 18:51:20 -0700

    STEP-03 - dialog structure

--- src/dg-dialog.ts
+++ src/dg-dialog.ts
@@ -13,7 +13,45 @@ const _shadowCss = css`
 		width: 22rem;
 
 		box-shadow: var(--elev-5);
+
+		display: grid;
+		grid-template-rows: 4rem auto 4rem;		
 	}
+
+	.dialog > * {
+		border: solid 1px red;
+	}	
+
+	header{
+		display: grid;
+		grid-template-columns: 1fr 2rem;
+		align-items: center;
+		padding-left: 1rem;
+	}	
+
+	section {
+		padding: 1rem; 
+		min-height: 4rem;
+		max-height: 20rem;
+		overflow: auto;
+	}	
+
+	footer{
+		display: grid;
+		grid-template-columns: 1fr auto;
+		align-items: center;
+		padding: 0 1rem;
+	}
+
+	.message{
+		font-size: .75rem;
+	}
+	
+	.buttons{
+		display: grid;
+		grid-auto-flow: column;
+		grid-gap: .5rem;
+	}			
 `
 
 @customElement('dg-dialog') // same as customElements.define('dg-dialog', DialogComponent)
@@ -24,7 +62,17 @@ class DialogComponent extends BaseHTMLElement { // extends HTMLElement
 
 		this.attachShadow({ mode: 'open' }).append(html`
 			<div class="dialog" part="dialog">
-				CONTENT
+				<header>
+					<span class="title">TITLE</span>
+					<c-ico href="#ico-close"></c-ico>
+				</header>
+				<section>
+					CONTENT
+				</section>	
+				<footer>
+					<span class="message">MESSAGE</span>
+					<span class="buttons">BUTTONS</span>
+				</footer>							
 			</div> 
 		`);
 
